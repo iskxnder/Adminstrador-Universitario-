24879751b92d9ce0d0b69c01a19af7e0aeb86d20
iskxnder <iskander1622@gmail.com>
2018-10-29 17:00:11 -0400

    segundo commit archivos que faltaron

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>Material Design Bootstrap</title>
+    <title>Portal Web Universitario</title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('portal/css/font-awesome.min.css') }}">
